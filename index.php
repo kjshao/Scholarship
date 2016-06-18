917c7e1693487d6a221fe9c1ce3636fc176598a1
Kejie Shao <kjshao@dicp.ac.cn>
2016-06-18 14:31:48 +0800

    new

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+  <title>奖学金评审系统</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="jquery/1.12.4/jquery.min.js"></script>
+  <script src="bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <p>奖学金评审系统</p>
+</body>
+</html>
